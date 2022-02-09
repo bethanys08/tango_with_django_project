ddac1dd4e319af5ce7694c4af9d14c2f204602a4
Bethany Shum <2567279s@student.gla.ac.uk>
2022-02-09 16:42:24 +0000

    nineth commit (chapter 9)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'rango:login'
